2212a0833b94a0da61547d6434a3209027694f1f
Hung Luu <hungluu111103@gmail.com>
2023-01-31 14:48:18 +0700

    commit

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function makeSignIn(uri, email, password) {
     fetch(uri + "/verification/", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "plain/text"
         },
         body: JSON.stringify({ email, password })
     })
