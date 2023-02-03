cd501f4bb7fd19ce5e78ffc59f0047a81a3c0563
Hung Luu <hungluu111103@gmail.com>
2023-02-03 20:18:59 +0700

    commit

--- script.js
+++ script.js
@@ -88,6 +88,7 @@ function makeSignIn(uri, email, password) {
         } 
         console.log(res.headers);
         // window.location.replace(MAIN_PAGE_URI);
+        document.cookie = "username=John Doe";
     })
     
 } 
