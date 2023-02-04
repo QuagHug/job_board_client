f7d86f4c448bcf0c3fe59694f126888c83f8a46c
Hung Luu <hungluu111103@gmail.com>
2023-02-04 21:57:49 +0700

    commit

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ function makeSignIn(uri, email, password) {
         headers: {
             "Content-Type": "application/json"
         },  
-        
+        credentials: "include",
         body: JSON.stringify({ email, password })
     })
     .then(async res => {
