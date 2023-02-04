5e13cf13702d39017f9cd356a73815b73e68e663
Hung Luu <hungluu111103@gmail.com>
2023-02-04 20:37:18 +0700

    commit

--- script.js
+++ script.js
@@ -86,9 +86,9 @@ function makeSignIn(uri, email, password) {
             errorText.setAttribute("class", "error-text");
             return error_card.appendChild(errorText);
         } 
-        // window.location.replace(MAIN_PAGE_URI);
         const expireDate = new Date((new Date()).getTime() + result.authentication.expire).toISOString();
         document.cookie = `jwt=${result.authentication.jwt}; expire=${expireDate}`;
+        window.location.replace(MAIN_PAGE_URI);
     })
     
 } 
